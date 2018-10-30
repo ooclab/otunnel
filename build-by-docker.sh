6b7aa070fb58b56758c006c2d496d8d26adc2c4d
gwind <lijian.gnu@gmail.com>
2018-10-30 14:12:42 +0800

    use golang:1.11 to build

--- build-by-docker.sh
+++ build-by-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 WORKDIR=/go/src/github.com/ooclab/otunnel
-docker run -it --rm -v $PWD:$WORKDIR -w $WORKDIR golang make $@
+docker run -it --rm -v $PWD:$WORKDIR -w $WORKDIR golang:1.11 make $@
